42d6375e21021454c0d563846cfc24524abfb6f5
Mozgushin Vitalii <mozhushyn.vitalii1124@vu.cdu.edu.ua>
2024-11-08 13:46:57 +0200

    Додано клас

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class MTZKIT
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("New Hello World");
+    }
+}
+    
